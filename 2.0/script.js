32970c180d548d861faaf17c030bfd718fd0c015
Stich.626 <12279@mail.ru>
2024-04-24 01:32:46 +0300

    Working

--- 2.0/script.js
+++ 2.0/script.js
@@ -1,3 +1,16 @@
+const data = [
+  {
+    menu: "Килограмм",
+    description:
+      'Введите цену за упаковку, её вес и нажмите на кнопку "Расчитать"',
+    priceTitle: "руб.",
+    edismTitle: "кг.",
+    formula: () => {
+      return ((price / count) * 1000).toFixed(2);
+    },
+  },
+];
+
 const menuButton = document.querySelector(".butter");
 const infoBtn = document.querySelector(".infoBtn");
 const closeButton = document.querySelector(".closeIcon");
@@ -27,3 +40,33 @@ closeButton.addEventListener("click", function (e) {
   about.classList.remove("open");
   lockContent();
 });
+
+function itogi() {
+  let price = document.querySelector("#price").value;
+  let count = document.querySelector("#count").value;
+  let itog = ((price / count) * 1000).toFixed(2);
+  console.log(itog);
+  document.querySelector("#price").value = "";
+  document.querySelector("#count").value = "";
+  document.getElementById("result").innerHTML =
+    '<span class="itog-itog">' +
+    new Intl.NumberFormat("ru-RU").format(itog) +
+    ' руб. </span><p class="itog-title">' +
+    "Цена за штуку" +
+    "</p>";
+
+  document
+    .getElementById("logi")
+    .insertAdjacentHTML(
+      "beforeend",
+      '<tr><td><div class="log-title">Цена за упаковку</div>' +
+        Intl.NumberFormat("ru-RU").format(price) +
+        ' руб.</td><td><div class="log-title">Упаковка</div>' +
+        Intl.NumberFormat("ru-RU").format(count) +
+        " руб. " +
+        '</td><td><div class="log-title"> Цена за килограмм' +
+        "</div>" +
+        Intl.NumberFormat("ru-RU").format(itog) +
+        " руб.</td></tr>"
+    );
+}
