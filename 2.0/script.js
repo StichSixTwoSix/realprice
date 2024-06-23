a6e62ede01bf8d8084d45c9a247e4e43d2b88dc4
Stich.626 <12279@mail.ru>
2024-06-24 00:31:29 +0300

    PreFinal

--- 2.0/script.js
+++ 2.0/script.js
@@ -6,17 +6,12 @@ const about = document.querySelector(".about");
 const body = document.querySelector(".body");
 const main = document.querySelector("#main");
 const footer = document.querySelector(".footer");
-// var calc = "kilo";
-
-function lockContent() {
-  body.classList.toggle("lock");
-  main.classList.toggle("blur");
-  footer.classList.toggle("blur");
-}
 
 menuButton.addEventListener("click", function (e) {
   menuBody.classList.toggle("open");
-  lockContent();
+  body.classList.toggle("lock");
+  main.classList.toggle("blur");
+  footer.classList.toggle("blur");
 });
 
 infoBtn.addEventListener("click", function (e) {
@@ -26,44 +21,15 @@ infoBtn.addEventListener("click", function (e) {
 
 closeButton.addEventListener("click", function (e) {
   about.classList.remove("open");
-  lockContent();
+  body.classList.remove("lock");
+  main.classList.remove("blur");
+  footer.classList.remove("blur");
 });
-
-function itogi() {
-  let price = document.querySelector("#price").value;
-  let count = document.querySelector("#count").value;
-  let itog = ((price / count) * 1000).toFixed(2);
-  console.log(itog);
-  console.log(calc);
-  document.querySelector("#price").value = "";
-  document.querySelector("#count").value = "";
-  document.getElementById("result").innerHTML =
-    '<span class="itog-itog">' +
-    new Intl.NumberFormat("ru-RU").format(itog) +
-    ' руб. </span><p class="itog-title">' +
-    "Цена за штуку" +
-    "</p>";
-
-  document
-    .getElementById("logi")
-    .insertAdjacentHTML(
-      "beforeend",
-      '<tr><td><div class="log-title">Цена за упаковку</div>' +
-        Intl.NumberFormat("ru-RU").format(price) +
-        ' руб.</td><td><div class="log-title">Упаковка</div>' +
-        Intl.NumberFormat("ru-RU").format(count) +
-        " руб. " +
-        '</td><td><div class="log-title"> Цена за килограмм' +
-        "</div>" +
-        Intl.NumberFormat("ru-RU").format(itog) +
-        " руб.</td></tr>"
-    );
-}
 
 function clears() {
   document.querySelector("#price").value = "";
   document.querySelector("#count").value = "";
-  document.getElementById("result").innerHTML = "";
+  document.querySelector("#result").innerHTML = "";
   document.querySelector("#logi").innerHTML = "";
 }
 
@@ -76,30 +42,118 @@ function cls() {
   document.querySelector("#tLit").classList.remove("active");
   document.querySelector("#tSht").classList.remove("active");
   document.querySelector("#tUpak").classList.remove("active");
+  menuBody.classList.remove("open");
+  body.classList.remove("lock");
+  main.classList.remove("blur");
+  footer.classList.remove("blur");
 }
 
-document.querySelector("#upak").addEventListener("click", function () {
+function kilo() {
   cls();
-  document.querySelector("#upack").classList.add("active");
-  document.querySelector("#tUpak").classList.add("active");
-  sht();
-});
-document.querySelector("#tUpak").addEventListener("click", function () {
+  document.getElementById("kg").classList.add("active");
+  document.getElementById("tKg").classList.add("active");
+  document.getElementById("result").innerHTML = `<span>
+    Введите цену за упаковку, её вес в граммах 
+    и нажмите на кнопку
+    "Рассчитать"</span>`;
+  document.getElementById("priceTitle").innerHTML = "Введите цену за упаковку";
+  document.getElementById("vesTitle").innerHTML = "Введите вес в граммах";
+  document.getElementById("knopka").name = "kilo";
+}
+function litre() {
   cls();
-  document.querySelector("#upak").classList.add("active");
-  document.querySelector("#tUpak").classList.add("active");
-  sht();
-});
-
-console.log(calc);
-
+  document.getElementById("lit").classList.add("active");
+  document.getElementById("tLit").classList.add("active");
+  document.getElementById("result").innerHTML = `<span>
+    Введите цену за упаковку, её объем в миллилитрах 
+    и нажмите на кнопку
+    "Рассчитать"</span>`;
+  document.getElementById("priceTitle").innerHTML = "Введите цену за упаковку";
+  document.getElementById("vesTitle").innerHTML = "Введите вес в миллилитрах";
+  document.getElementById("knopka").name = "litre";
+}
 function sht() {
-  document.getElementById("result").innerHTML = `<span
-          >Введите цену за килограмм (литр), вес (объём) 
-          упаковки и нажмите на кнопку
-          "Рассчитать"</span>`;
+  cls();
+  document.getElementById("sht").classList.add("active");
+  document.getElementById("tSht").classList.add("active");
+  document.getElementById("result").innerHTML = `<span>
+    Введите цену за упаковку, количество 
+    и нажмите на кнопку
+    "Рассчитать"</span>`;
+  document.getElementById("priceTitle").innerHTML = "Введите цену за упаковку";
+  document.getElementById("vesTitle").innerHTML = "Введите количество в штуках";
+  document.getElementById("knopka").name = "sht";
+}
+function upak() {
+  cls();
+  document.getElementById("upak").classList.add("active");
+  document.getElementById("tUpak").classList.add("active");
+  document.getElementById("result").innerHTML = `<span>
+    Введите цену за килограмм (литр), вес (объём) 
+    упаковки и нажмите на кнопку
+    "Рассчитать"</span>`;
   document.getElementById("priceTitle").innerHTML = "Введите цену за килограмм";
   document.getElementById("vesTitle").innerHTML = "Введите вес в граммах";
-  var calc = "upack";
-  console.log(calc);
+  document.getElementById("knopka").name = "upak";
+}
+
+function itogi() {
+  let countType = document.getElementById("knopka").name;
+  let price = document.querySelector("#price").value;
+  let count = document.querySelector("#count").value;
+  var resultat;
+
+  if ((price > 0) | (count > 0)) {
+    switch (countType) {
+      case "kilo":
+        resultat = ((price / count) * 1000).toFixed(2);
+        var logTitle = "Цена за килограмм";
+        var logSI = " г.";
+        break;
+      case "litre":
+        resultat = ((price / count) * 1000).toFixed(2);
+        var logTitle = "Цена за литр";
+        var logSI = " мл.";
+        break;
+      case "sht":
+        resultat = (price / count).toFixed(2);
+        var logTitle = "Цена за штуку";
+        var logSI = " шт.";
+        break;
+      case "upak":
+        resultat = price;
+        price = ((resultat * count) / 1000).toFixed(2);
+        var logTitle = "Цена за килограмм";
+        var logSI = " уп.";
+        break;
+    }
+
+    document.getElementById("result").innerHTML =
+      '<span class="itog-itog">' +
+      new Intl.NumberFormat("ru-RU").format(resultat) +
+      ' руб. </span><p class="itog-title">' +
+      logTitle +
+      "</p>";
+
+    document
+      .getElementById("logi")
+      .insertAdjacentHTML(
+        "beforeend",
+        '<tr><td><div class="log-title"> Цена за упаковку' +
+          "</div>" +
+          Intl.NumberFormat("ru-RU").format(price) +
+          ' руб.</td><td><div class="log-title">Упаковка</div>' +
+          Intl.NumberFormat("ru-RU").format(count) +
+          logSI +
+          '</td><td><div class="log-title">' +
+          logTitle +
+          "</div>" +
+          Intl.NumberFormat("ru-RU").format(resultat) +
+          " руб.</td></tr>"
+      );
+    document.querySelector("#price").value = "";
+    document.querySelector("#count").value = "";
+  } else {
+    alert("Проверьте ввод!");
+  }
 }
