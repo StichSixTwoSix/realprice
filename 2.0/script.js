86e76efe121932850a3bb331929252e323a6250f
StichSixTwoSix <72442001+StichSixTwoSix@users.noreply.github.com>
2024-06-23 18:22:54 +0300

    Add files via upload

--- 2.0/script.js
+++ 2.0/script.js
@@ -1,16 +1,3 @@
-const data = [
-  {
-    menu: "Килограмм",
-    description:
-      'Введите цену за упаковку, её вес и нажмите на кнопку "Расчитать"',
-    priceTitle: "руб.",
-    edismTitle: "кг.",
-    formula: () => {
-      return ((price / count) * 1000).toFixed(2);
-    },
-  },
-];
-
 const menuButton = document.querySelector(".butter");
 const infoBtn = document.querySelector(".infoBtn");
 const closeButton = document.querySelector(".closeIcon");
@@ -19,6 +6,7 @@ const about = document.querySelector(".about");
 const body = document.querySelector(".body");
 const main = document.querySelector("#main");
 const footer = document.querySelector(".footer");
+// var calc = "kilo";
 
 function lockContent() {
   body.classList.toggle("lock");
@@ -32,8 +20,8 @@ menuButton.addEventListener("click", function (e) {
 });
 
 infoBtn.addEventListener("click", function (e) {
+  menuBody.classList.remove("open");
   about.classList.add("open");
-  lockContent();
 });
 
 closeButton.addEventListener("click", function (e) {
@@ -46,6 +34,7 @@ function itogi() {
   let count = document.querySelector("#count").value;
   let itog = ((price / count) * 1000).toFixed(2);
   console.log(itog);
+  console.log(calc);
   document.querySelector("#price").value = "";
   document.querySelector("#count").value = "";
   document.getElementById("result").innerHTML =
@@ -69,4 +58,48 @@ function itogi() {
         Intl.NumberFormat("ru-RU").format(itog) +
         " руб.</td></tr>"
     );
+}
+
+function clears() {
+  document.querySelector("#price").value = "";
+  document.querySelector("#count").value = "";
+  document.getElementById("result").innerHTML = "";
+  document.querySelector("#logi").innerHTML = "";
+}
+
+function cls() {
+  document.querySelector("#kg").classList.remove("active");
+  document.querySelector("#lit").classList.remove("active");
+  document.querySelector("#sht").classList.remove("active");
+  document.querySelector("#upak").classList.remove("active");
+  document.querySelector("#tKg").classList.remove("active");
+  document.querySelector("#tLit").classList.remove("active");
+  document.querySelector("#tSht").classList.remove("active");
+  document.querySelector("#tUpak").classList.remove("active");
+}
+
+document.querySelector("#upak").addEventListener("click", function () {
+  cls();
+  document.querySelector("#upack").classList.add("active");
+  document.querySelector("#tUpak").classList.add("active");
+  sht();
+});
+document.querySelector("#tUpak").addEventListener("click", function () {
+  cls();
+  document.querySelector("#upak").classList.add("active");
+  document.querySelector("#tUpak").classList.add("active");
+  sht();
+});
+
+console.log(calc);
+
+function sht() {
+  document.getElementById("result").innerHTML = `<span
+          >Введите цену за килограмм (литр), вес (объём) 
+          упаковки и нажмите на кнопку
+          "Рассчитать"</span>`;
+  document.getElementById("priceTitle").innerHTML = "Введите цену за килограмм";
+  document.getElementById("vesTitle").innerHTML = "Введите вес в граммах";
+  var calc = "upack";
+  console.log(calc);
 }
